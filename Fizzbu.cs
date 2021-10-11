564e0879f83416ad0631ac4b07b1a35576eb9f30
Seth-Race <sethrace@gmail.com>
2021-10-10 20:37:45 -0400

    Family Commit

---
+++ Fizzbu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FIZZBUZZ {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+           
+                for (var idx = 1; idx <= 30; idx++) {
+                if (idx % 3 == 0 && idx % 5 == 0) {
+                    Console.Write("FIZZBUZZ");
+                }
+                else if (idx % 3 == 0) {
+                    Console.Write("FIZZ");
+                    {
+                        if (idx % 5 == 0) {
+                            Console.Write("Buzz");
+                        }
+                    }
+                }
+                   
+            }
+
+        }
+    }
+}
---
+++ TestConsoleAppSolution/TestConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("To the C# World...");
+
+            int NumberOfStudents = 17;
+            var TotalInClass = NumberOfStudents + 2;
+            Console.WriteLine($"Number of students is {NumberOfStudents}, total is {TotalInClass}");
+            var MyName = "Chauntae";
+            Console.WriteLine($"{MyName} is one of the {NumberOfStudents} students");
+            
+            Console.WriteLine($"There are {TotalInClass - NumberOfStudents} non-students in the class");
+
+
+
+        }
+    }
+}
+ 9
